4e6434c44f086c96e5d5809a542e156033cf1c27
DiegoRomero <daromeroromero14@gmail.com>
2026-03-20 15:01:18 -0500

    funcionalidad usuarios

---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuario.controller');
+
+router.post('/', usuarioController.crearUsuario);
+router.get('/', usuarioController.listarUsuarios);
+router.get('/:id', usuarioController.obtenerUsuarioPorId);
+router.patch('/:id/estado', usuarioController.cambiarEstadoUsuario);
+router.get('/:id/saldo', usuarioController.obtenerSaldoUsuario);
+
+module.exports = router;
